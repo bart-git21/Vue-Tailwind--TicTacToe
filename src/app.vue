d9c30e33181feca2b28211175b87066368969302
gitName <bartk@list.ru>
2025-02-16 15:31:32 +0300

    feat: init the board value and change the player

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,37 @@
-<script lang="ts" setup>
+<script setup>
+import { ref, computed } from "vue";
+const player = ref("X");
+const board = ref(["", "", "", "", "", "", "", "", ""]);
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+      return squares[a];
+    return null;
+  }
+};
+
+const winner = computed(() => calculateWinner(board.value));
+
+const makeMove = (id) => {
+  if (winner.value) return;
+  if (board.value[id]) return;
+  board.value[id] = player.value;
+  player.value = player.value === "X" ? "O" : "X";
+};
 </script>
 
-<template>
-</template>
+<template></template>
 
-<style scoped>
-</style>
+<style scoped></style>
